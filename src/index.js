49fcb1d819489ba6544802863d51b8b70157f725
AriCohelo <ariel.cohelo@gmail.com>
2024-04-24 01:54:41 +0200

    setted up the HTML for Js form validation and the way to acces forms in JS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import './main.scss';
+
+let form = document.forms.myForm;
+let firstNameInput = document.forms.myForm[0];
+let lastNameInput = document.forms.myForm[1];
+let emailInput = document.forms.myForm[2];
+let passwordInput = document.forms.myForm[3];
+let submitButton = document.forms.myForm[4];
+
+console.log(form, firstNameInput, lastNameInput, emailInput, passwordInput);
+submitButton.addEventListener('click', (e) => {
+	e.preventDefault();
+	console.log(firstNameInput.checkValidity());
+});
