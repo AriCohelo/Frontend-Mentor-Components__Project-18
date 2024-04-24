d1f74e0a6c4c5045dd74572e84715e06b5209ee1
AriCohelo <ariel.cohelo@gmail.com>
2024-04-25 01:42:13 +0200

    almost done,  just have to adjust mediaquery sizes, but the JS and error messages are finished

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import './main.scss';
 
-let form = document.forms.myForm;
 let errorMessage = document.querySelectorAll('.errorMessage');
 let firstNameInput = document.forms.myForm[0];
 let firstNameInputError = document.forms.myForm[1];
 let lastNameInput = document.forms.myForm[2];
 let lastNameInputError = document.forms.myForm[3];
-
-// let emailInput = document.forms.myForm[3];
-// let passwordInput = document.forms.myForm[4];
-let submitButton = document.forms.myForm[6];
+let emailInput = document.forms.myForm[4];
+let emailInputError = document.forms.myForm[5];
+let passwordInput = document.forms.myForm[6];
+let passwordInputError = document.forms.myForm[7];
+let submitButton = document.forms.myForm[8];
 
 console.log(document.forms);
+
+//
+
+// TOGGLING CLASSES CREATES THE PROBLEM THAT IF THE FORM IS NOT FILLED PROPERLY AND PRESS
+// THE SUBMIT BUTTON THE ERRORS APPEAR BUT WHEN THE SUBMIT BUTTON IS PRESSED AGAIN, THE
+// ERRORS MESSAGES DISSAPEAR EVEN IF THE FORM WAS NOT FILLED PROPERLY
+
+//
 submitButton.addEventListener('click', (e) => {
 	if (
 		!firstNameInput.value &&
@@ -19,12 +27,28 @@ submitButton.addEventListener('click', (e) => {
 		!emailInput.value & !passwordInput.value
 	)
 		return;
-	if (!firstNameInput.checkValidity() || !lasttNameInput.checkValidity()) {
+	if (
+		!firstNameInput.checkValidity() ||
+		!lastNameInput.checkValidity() ||
+		!emailInput.checkValidity() ||
+		!passwordInput.checkValidity()
+	) {
 		e.preventDefault();
 		firstNameInput.classList.toggle('hidden');
 		firstNameInputError.classList.toggle('hidden');
+		firstNameInputError.value = firstNameInput.value;
+
 		lastNameInput.classList.toggle('hidden');
 		lastNameInputError.classList.toggle('hidden');
+		lastNameInputError.value = lastNameInput.value;
+
+		emailInput.classList.toggle('hidden');
+		emailInputError.classList.toggle('hidden');
+		emailInputError.value = emailInput.value;
+
+		passwordInput.classList.toggle('hidden');
+		passwordInputError.classList.toggle('hidden');
+
 		errorMessage.forEach((element) => {
 			element.classList.toggle('hidden');
 		});
