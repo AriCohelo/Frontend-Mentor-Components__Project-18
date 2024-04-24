6362e2fc995ed671309b5de5b29d4b5db4b148fc
AriCohelo <ariel.cohelo@gmail.com>
2024-04-24 21:21:32 +0200

    solved the ::after with no after but with a<p> with a hidden class

--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
 import './main.scss';
 
 let form = document.forms.myForm;
+let errorMessage = document.querySelectorAll('.errorMessage');
 let firstNameInput = document.forms.myForm[0];
-let lastNameInput = document.forms.myForm[1];
-let emailInput = document.forms.myForm[2];
-let passwordInput = document.forms.myForm[3];
-let submitButton = document.forms.myForm[4];
+let firstNameInputError = document.forms.myForm[1];
+let lastNameInput = document.forms.myForm[2];
+let lastNameInputError = document.forms.myForm[3];
 
-console.log(form, firstNameInput, lastNameInput, emailInput, passwordInput);
+// let emailInput = document.forms.myForm[3];
+// let passwordInput = document.forms.myForm[4];
+let submitButton = document.forms.myForm[6];
+
+console.log(document.forms);
 submitButton.addEventListener('click', (e) => {
-	e.preventDefault();
-	console.log(firstNameInput.checkValidity());
+	if (
+		!firstNameInput.value &&
+		!lastNameInput.value &&
+		!emailInput.value & !passwordInput.value
+	)
+		return;
+	if (!firstNameInput.checkValidity() || !lasttNameInput.checkValidity()) {
+		e.preventDefault();
+		firstNameInput.classList.toggle('hidden');
+		firstNameInputError.classList.toggle('hidden');
+		lastNameInput.classList.toggle('hidden');
+		lastNameInputError.classList.toggle('hidden');
+		errorMessage.forEach((element) => {
+			element.classList.toggle('hidden');
+		});
+	}
 });
